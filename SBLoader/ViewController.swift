9b33d198e96ef2075ba65067e390d99c69153552
jasmo2 <jasmo2.com@gmail.com>
2017-03-05 13:56:07 -0500

    implement delegate; animateLabel

--- SBLoader/ViewController.swift
+++ SBLoader/ViewController.swift
@@ -39,21 +39,29 @@ class ViewController: UIViewController, HolderViewDelegate {
   }
   
   func animateLabel() {
-
+    holderView.removeFromSuperview()
+    view.backgroundColor = Colors.red
+    
+    // 2
+    let label: UILabel = UILabel(frame: view.frame)
+    label.textColor = Colors.black
+    label.font = UIFont(name: "HelveticaNeue-Thin", size: 170.0)
+    label.textAlignment = NSTextAlignment.center
+    label.text = "S"
+    label.transform = label.transform.scaledBy(x: 0.25, y: 0.25)
+    view.addSubview(label)
+    
+    // 3
+    UIView.animate(
+        withDuration: 0.4, delay: 0.0,
+        usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1,
+        options: UIViewAnimationOptions.curveEaseInOut,
+        animations: ({
+            label.transform = label.transform.scaledBy(x: 4.0, y: 4.0)
+        }),
+        completion: nil
+    )
   }
   
-  func addButton() {
-    let button = UIButton()
-    button.frame = CGRect(x: 0.0, y: 0.0, width: view.bounds.width, height: view.bounds.height)
-    button.addTarget(self, action: #selector(ViewController.buttonPressed(_:)), for: .touchUpInside)
-    view.addSubview(button)
-  }
-  
-  func buttonPressed(_ sender: UIButton!) {
-    view.backgroundColor = Colors.white
-    view.subviews.map({ $0.removeFromSuperview() })
-    holderView = HolderView(frame: CGRect.zero)
-    addHolderView()
-  }
 }
 
