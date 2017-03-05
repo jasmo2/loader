ef8b44e7d8a842701c19178c66223df8be124306
jasmo2 <jasmo2.com@gmail.com>
2017-03-05 13:53:07 -0500

    ADD COLOR BLACK

--- SBLoader/Colors.swift
+++ SBLoader/Colors.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 struct Colors {
-  static let blue = UIColor(red: 46.0 / 255.0, green: 117.0 / 255.0, blue: 146.0 / 255.0, alpha: 1.0)
-  static let red = UIColor(red: 209.0 / 255.0, green: 42.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
-  static let white = UIColor.white
-  static let clear = UIColor.clear
+    static let blue = UIColor(red: 46.0 / 255.0, green: 117.0 / 255.0, blue: 146.0 / 255.0, alpha: 1.0)
+    static let red = UIColor(red: 209.0 / 255.0, green: 42.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
+    static let white = UIColor.white
+    static let black = UIColor.black
+    static let clear = UIColor.clear
 }
